473ca7cb6d7246076d1dad1484c7391a2c842b92
arata-haruto <138739158+arata-haruto@users.noreply.github.com>
2026-02-19 09:32:23 +0900

    環境構築１

---
+++ GameJam/AppMain.cpp
@@ -0,0 +1,14 @@
+#include "DxLib.h"
+#include "Utility/Application.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+
+	Application* a = Application::GetInstance();
+
+	a->WakeUp();
+	a->Run();
+
+	return D_SUCCESS;
+
+}
