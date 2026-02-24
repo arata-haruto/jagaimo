21e89ea716d0138897bac4b9500637af36c822a2
sakana-pixel <rento-higa-k25f@stu.kbc.ac.jp>
2026-02-24 14:08:52 +0900

    Rento

--- GameJam/SceneManager/Title/Title.cpp
+++ GameJam/SceneManager/Title/Title.cpp
@@ -11,6 +11,7 @@ int TitleBGM;
 int PushSE;
 int Movement;
 int Title_image;
+int Logo_image;
 
 /// <summary>
 /// 初期化処理
@@ -22,6 +23,7 @@ void TitleInit(void)
 	PushSE = LoadSoundMem("sounds/SE/buutton.wav");
 	Movement = LoadSoundMem("sounds/SE/menu_move.wav");
 	Title_image = LoadGraph("images/background/bg_natural_mori.jpg");
+	Logo_image = LoadGraph("images/Title/Titlelogo.png");
 	cursor_number = 0;//矢印の位置を一番目にする
 	InitFlag();//フラグ初期化を呼び出す
 
@@ -100,8 +102,8 @@ void TitleDraw(void)
 {
 	DrawGraph(0, 0, Title_image, TRUE);
 	//文を描画する
-	DrawString(200, 200, "タイトル",Cr);
-	DrawString(200, 300, "スペースでインゲーム",Cr);
+	DrawGraph(-680, -280, Logo_image,TRUE);
+	DrawString(200, 450, "スペースでインゲーム",Cr);
 	DrawString(200, 500, "ヘルプ", Cr);
 	DrawString(200, 550, "ランキング", Cr);
 	DrawString(200, 600, "ゲームを終わる", Cr);
@@ -110,7 +112,7 @@ void TitleDraw(void)
 	if (blink_flag == TRUE)
 	{
 		int cy = cursor_number * 50;//カーソル番号ごとに縦に動かす
-		DrawTriangle(180, 495 + cy, 190, 505 + cy, 180, 515 + cy, GetColor(255, 255, 255), TRUE);//矢印を描画する
+		DrawTriangle(180, 495 + cy, 190, 505 + cy, 180, 515 + cy, GetColor(255, 0, 0), TRUE);//矢印を描画する
 	}
 
 }
